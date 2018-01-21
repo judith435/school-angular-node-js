e14a2c3acec932048e7eedcb1adee0d545237b01
יהודית אילסון <JudithI@kkl.org.il>
2018-01-21 13:39:31 +0200

    sava

--- client/course/course.controller.js
+++ client/course/course.controller.js
@@ -1,9 +1,10 @@
 schoolApp.controller('courseController', function handleCourses($scope, configSettings, courseService, studentService, canvasService) {
     $scope.courseSummary = $scope.selectedCourse.courseName + ', ' + $scope.selectedCourse.numberOfStudentsForCourse + ' students';
-   
-   
+    $scope.dodo = 'gunibush';
     var lala = $scope.selectedCourse;
     console.log(JSON.stringify(lala));
+    console.log($scope.dodo);
+
    // alert (JSON.stringify($scope.courseSelected));
  
 });
